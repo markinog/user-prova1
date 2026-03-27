547274c20673621701b582923ee1df9672f79738
markinog <marcussla9@gmail.com>
2026-03-27 09:10:08 -0300

    ajuste para email ser unico no banco

--- src/main/java/com/ecommerce/user/entity/User.java
+++ src/main/java/com/ecommerce/user/entity/User.java
@@ -1,16 +1,26 @@
 package com.ecommerce.user.entity;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "usuaroio")
+@Document(collection = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
     @Schema(hidden = true)
     private String id;
     private String nome;
+    @Indexed(unique = true)
     private String email;
 
 }
